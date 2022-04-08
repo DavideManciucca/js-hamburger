8b540f6dd709124ffc6fd98094aaf982687574f9
DavideManciucca <davidemanciucca@gmail.com>
2022-04-08 09:13:14 +0200

    esercizio terminato

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const hamburgerIcon = document.querySelector('.fas.fa-bars');
 
 const closeIcon = document.querySelector('.close');
 
+hamburgerIcon.addEventListener('click', function(){
+  hamburgerMenu.classList.add('active');
+});
+
+closeIcon.addEventListener('click', function(){
+  hamburgerMenu.classList.remove('active');
+});
