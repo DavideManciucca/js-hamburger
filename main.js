f96c099522e86e3b1df6139bb5a4c8d5a59be838
DavideManciucca <davidemanciucca@gmail.com>
2022-04-08 09:06:45 +0200

    primo commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+const hamburgerIcon = document.querySelector('.fas.fa-bars');
+
+const closeIcon = document.querySelector('.close');
+
